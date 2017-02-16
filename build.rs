a5a9f4406e135b169eb2ed2e4c14fb4b7db4272c
Ryan1729 <Ryan1729@gmail.com>
2017-02-15 20:53:47 -0700

    copy files from https://github.com/Ryan1729/st-plus-some-patches and add double_input to st.c and get it to build
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
 extern crate gcc;
 
 fn main() {
-    gcc::Config::new().file("src/st.c").compile("libst.a");
+    gcc::Config::new()
+        .include("/usr/include")
+        .include("/usr/X11R6/include")
+        .include("/usr/include/freetype2")
+        .flag("-L/usr/lib -lc -L/usr/X11R6/lib -lm -lrt -lX11 -lutil -lXft -lfontconfig -lfreetype -lfreetype")
+        .file("src/st.c").compile("libst.a");
+    
+    //~ -g -std=c99 -pedantic -Wall -Wvariadic-macros -Os -I. -I -I -I/usr/include/freetype2 -I/usr/include/freetype2 -DVERSION="0.6" -D_XOPEN_SOURCE=600
+
 }
