1fa70c32db07d5cccd5d55e014725d316111bce5
Ryan1729 <Ryan1729@gmail.com>
2017-03-01 15:01:55 -0700

    clean up build script
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- build.rs
+++ build.rs
@@ -1,23 +1,6 @@
 extern crate gcc;
 
 fn main() {
-    // gcc::Config::new()
-    //     .include("/usr/include")
-    //     .include("/usr/X11R6/include")
-    //     .include("/usr/include/freetype2")
-    //     .flag("-D_XOPEN_SOURCE=600")
-    //     .flag("-L/usr/lib")
-    //     .flag("-lc")
-    //     .flag("-L/usr/X11R6/lib")
-    //     .flag("-lm")
-    //     .flag("-lrt")
-    //     .flag("-lX11")
-    //     .flag("-lutil")
-    //     .flag("-lXft")
-    //     .flag("-lfontconfig")
-    //     .flag("-lfreetype")
-    //     .file("src/st.c")
-    //     .compile("libst1.a");
     gcc::Config::new()
         .include("src")
         .flag("-g")
@@ -26,16 +9,6 @@ fn main() {
         .flag("-I/usr/X11R6/include")
         .flag("-I/usr/include/freetype2")
         .flag("-D_XOPEN_SOURCE=600")
-        .flag("-L/usr/lib")
-        .flag("-lc")
-        .flag("-L/usr/X11R6/lib")
-        .flag("-lm")
-        .flag("-lrt")
-        .flag("-lX11")
-        .flag("-lutil")
-        .flag("-lXft")
-        .flag("-lfontconfig")
-        .flag("-lfreetype")
         .file("src/st.c")
         .compile("libst1.a");
 
